3525b6964134a68d92ea721ef782af73aaca64e5
Gavin McNatt <gmcnatt14@students.desu.edu@gavins-macbook-pro.local>
2019-01-22 22:01:03 -0500

    Initial Commit

---
+++ MidProgramTipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidProgramTipApp
+//
+//  Created by Gavin McNatt on 1/22/19.
+//  Copyright © 2019 Gavin McNatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
